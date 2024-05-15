70bd15713a5c0e2a2a74833be9909b7bbe417190
marcosnathan <mnengcomp@gmail.com>
2024-05-15 00:10:24 -0300

    dio condicionais encadeadas

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,8 +1,10 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 6;
+        int nota = 8;
         if (nota >= 7)
             System.out.println("Aprovado");
+        else if (nota >= 5 && nota < 7)
+            System.out.println("Recuperação");
         else
             System.out.println("Reprovado");
     }
